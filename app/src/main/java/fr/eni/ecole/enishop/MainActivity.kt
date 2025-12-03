52cf8343aece26e17426b2f240969e2884799452
Quentin PRIGENT <qprigent@campus-eni.fr>
2025-12-03 13:55:14 +0100

    Exercice done

--- app/src/main/java/fr/eni/ecole/enishop/MainActivity.kt
+++ app/src/main/java/fr/eni/ecole/enishop/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,9 +15,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import fr.eni.ecole.enishop.dao.memory.ArticleDaoMemoryImpl
+import fr.eni.ecole.enishop.bo.Article
 import fr.eni.ecole.enishop.repository.ArticleRepository
+import fr.eni.ecole.enishop.ui.screens.ArticleDetailsScreen
 import fr.eni.ecole.enishop.ui.theme.ENIShopTheme
+import fr.eni.ecole.enishop.ui.shared.TopBar
 import java.util.Date
 
 private const val TAG = "MAIN SCREEN"
@@ -29,46 +32,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val articles =
-
             ENIShopTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
-                        TopAppBar(
-                            title = { TopBar(Modifier) }
-                        )
+                        TopBar()
                     }
                 ) { innerPadding ->
-                    ArticleDetails(
-                        name = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-                        description = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-                        price = 109.95,
-                        urlImage = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-                        date = Date(),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ENIShopApp(modifier = Modifier.padding(innerPadding))
                 }
             }
-            var articleRepository = ArticleRepository()
-            var article = articleRepository.getArticle(1);
-            Log.i(TAG, "L'article $article a été récupéré")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ENIShopApp(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ENIShopTheme {
-        Greeting("Android")
+
+    Column(
+        modifier = modifier
+    ) {
+        ArticleDetailsScreen(
+            articleId = 1
+        )
     }
 }
---
+++ app/src/main/java/fr/eni/ecole/enishop/utils/Extension.kt
@@ -0,0 +1,13 @@
+package fr.eni.ecole.enishop.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+fun Date.toFrenchFormat(): String {
+    val formatter = SimpleDateFormat("dd/MM/yyyy")
+    return formatter.format(this)
+}
+
+fun Double.toPriceFormat(): String {
+    return String.format("%.2f", this)
+}
