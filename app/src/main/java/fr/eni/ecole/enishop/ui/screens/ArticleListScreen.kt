b4593d33ac53782dc00360898da57965b8634439
Quentin PRIGENT <qprigent@campus-eni.fr>
2025-12-05 15:57:51 +0100

    Optimize imports

--- app/src/main/java/fr/eni/ecole/enishop/ui/screens/ArticleListScreen.kt
+++ app/src/main/java/fr/eni/ecole/enishop/ui/screens/ArticleListScreen.kt
@@ -22,19 +22,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -173,7 +169,6 @@ fun ArticleItem(article: Article) {
                     .padding(8.dp)
 
             )
-
             Text(
                 text = article.name,
                 style = MaterialTheme.typography.titleMedium,
